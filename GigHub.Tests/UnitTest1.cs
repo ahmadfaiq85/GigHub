70422afc0b17c65b9fd8adc498a1441183bbe3c3
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-01-10 14:09:29 -0500

    Start writing tests

--- GigHub.Tests/UnitTest1.cs
+++ GigHub.Tests/UnitTest1.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GigHub.Core.Models;
+using FluentAssertions;
+using System.Linq;
 
 namespace GigHub.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+         
+
     }
 }
