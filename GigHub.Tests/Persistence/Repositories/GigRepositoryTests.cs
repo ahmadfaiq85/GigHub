dc38629bcb06d8f5fc81cf46a22c62459edc7cce
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-01-12 10:49:16 -0500

    Write Unit tests for GigsUserAttending

--- GigHub.Tests/Persistence/Repositories/GigRepositoryTests.cs
+++ GigHub.Tests/Persistence/Repositories/GigRepositoryTests.cs
@@ -15,15 +15,18 @@ namespace GigHub.Tests.Persistence.Repositories
     {
         private GigRepository _repository;
         private Mock<DbSet<Gig>> _mockGigs;
-         
+        private Mock<DbSet<Attendance>> _mockAttendances;
+
         [TestInitialize]
         public void TestInitialize()
         {
-            _mockGigs = new Mock<DbSet<Gig>>();
+            _mockGigs = new Mock<DbSet<Gig>>();            
+            _mockAttendances = new Mock<DbSet<Attendance>>();
 
             var mockContext = new Mock<IApplicationDbContext>();
 
             mockContext.SetupGet(c => c.Gigs).Returns(_mockGigs.Object);
+            mockContext.SetupGet(c => c.Attendances).Returns(_mockAttendances.Object);
 
             _repository = new GigRepository(mockContext.Object); 
         }
@@ -68,12 +71,50 @@ namespace GigHub.Tests.Persistence.Repositories
         [TestMethod]
         public void GetUpcomingGigsByArtist_GigIsForTheGivenArtistAndIsInTheFuture_ShouldBeReturned()
         {
-
             var gig = new Gig() { DateTime = DateTime.Now.AddDays(1), ArtistId = "1" };
 
             _mockGigs.SetSource(new[] { gig });
 
             var gigs = _repository.GetUpcomingGigsByArtist(gig.ArtistId);
+
+            gigs.Should().Contain(gig);
+        }
+
+        [TestMethod]
+        public void GetGigsUserAttending_GigIsInThePast_ShouldNotBeReturned()
+        {
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(-1) };
+            var attendance = new Attendance { Gig = gig, AttendeeId = "1" };
+
+            _mockAttendances.SetSource(new[] { attendance });
+
+            var gigs = _repository.GetGigsUserAttending(attendance.AttendeeId);
+
+            gigs.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetGigsUserAttending_AttendanceForADifferentUser_ShouldNotBeReturned()
+        {
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(1) };
+            var attendance = new Attendance { Gig = gig, AttendeeId = "1" };
+
+            _mockAttendances.SetSource(new[] { attendance });
+
+            var gigs = _repository.GetGigsUserAttending(attendance.AttendeeId + "-");
+
+            gigs.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetGigUserAttending_UpcomingGigUserAttending_ShouldBeReturned()
+        {
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(1) };
+            var attendance = new Attendance { Gig = gig, AttendeeId = "1" };
+
+            _mockAttendances.SetSource(new[] { attendance });
+
+            var gigs = _repository.GetGigsUserAttending(attendance.AttendeeId );
 
             gigs.Should().Contain(gig);
         }
