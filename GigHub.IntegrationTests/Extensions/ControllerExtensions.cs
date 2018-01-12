de33c43c04e01296f3be3f2e5b5d897d1568af91
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-01-12 15:35:39 -0500

    Mock user in Mvc controller for integration test

---
+++ GigHub.IntegrationTests/Extensions/ControllerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Mvc;
+using Moq;
+using System.Web;
+
+namespace GigHub.IntegrationTests.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static void MockCurrentUser(this Controller controller, string userId, string username)
+        {
+            var identity = new GenericIdentity(username);
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", username));
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", userId));
+
+            var principal = new GenericPrincipal(identity, null);
+
+            var mockHttpContext = new Mock<HttpContextBase>();
+            mockHttpContext.SetupGet(c => c.User).Returns(principal);
+
+            var mockControllercontext = new Mock<ControllerContext>();
+            mockControllercontext.SetupGet(c => c.HttpContext).Returns(mockHttpContext.Object);
+
+            controller.ControllerContext = mockControllercontext.Object; 
+        }
+    }
+}
--- GigHub.IntegrationTests/GobalSetup.cs
+++ GigHub.IntegrationTests/GobalSetup.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
+using GigHub.Persistence;
+using GigHub.Core.Models;
 
 namespace GigHub.IntegrationTests
 {
@@ -15,10 +17,29 @@ namespace GigHub.IntegrationTests
         [SetUp]
         public void Setup()
         {
+            MigrateDbToLatestVersion();
+            Seed();
+        }
+
+        public static void MigrateDbToLatestVersion()
+        {
             var configuration = new Migrations.Configuration();
             var migrator = new DbMigrator(configuration);
 
             migrator.Update();
+        }
+
+        public void Seed()
+        {
+            var context = new ApplicationDbContext();
+
+            if (context.Users.Any())
+                return;
+
+            context.Users.Add(new ApplicationUser { UserName = "user1", Name = "user1", Email = "-", PasswordHash = "-" });
+            context.Users.Add(new ApplicationUser { UserName = "user2", Name = "user2", Email = "-", PasswordHash = "-" });
+
+            context.SaveChanges();
         }
     }
 }
